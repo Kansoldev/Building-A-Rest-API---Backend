81b6b9821f2d5cc2e072a263bdbca26a8b9e527b
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-09-01 19:36:58 +0100

    This project is a movie watchlist API. Putting all the routes in one file as I learnt before does not make sense because the routes will be many. Hence, why I need to store all my routes in a `routes` folder, and break the routes for the API into their own file.
    For this project, we are dealing with 4 main parts as pertaining the routes
    - Auth
    - Users
    - Movies
    - Watchlist

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import movieRoute from "./routes/movieRoutes.js";
 
 const app = express();
+app.use("/movie", movieRoute);
+
 const PORT = 5001;
 
 app.listen(PORT, () => console.log("App is running"));
